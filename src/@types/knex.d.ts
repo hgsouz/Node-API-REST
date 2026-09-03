19fde601739eb3be32c0c030a8f29d6e9d1e8618
Hugo Jesus <hugo.souza43@hotmail.com>
2026-09-03 01:26:24 -0300

    feat: adding more routes and notes to improve our API

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -3,6 +3,9 @@
 
 import { Knex } from "knex";
 
+/* Aqui estamos seguindo a orientação da documentação do Knex para sua tipagem 
+quando utilizado com TS, pois assim temos ctz apenas os campos certos estão
+sendo chamados no metodo */
 declare module "knex/types/tables" {
   export interface Tables {
     transaction: {
--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -1,14 +1,61 @@
 import type { FastifyInstance } from "fastify";
-import { z } from "zod";
+import { string, z } from "zod";
 import { randomUUID } from "node:crypto";
 import { knexDb } from "../database.js";
 
 // Definição do plugin presente no fastify, o que nos premite criar a rota aqui e utilizar em nosso app
 export async function transactionRoutes(app: FastifyInstance) {
+  // ------------------------------------------------------------------------------------------------
   // Verbos HTTP: GET, POST, PUT, PATCH, DELETE
 
-  /* Para a criação de rotas com fastify passamos apenas o verbo em função, passando o primeiro 
-    parâmetro como o destino e o segundo um callback com nosso retorno. */
+  /* 
+  Para a criação de rotas com fastify passamos apenas o verbo em função, passando o primeiro 
+  parâmetro como o destino e o segundo um callback com nosso retorno. 
+  */
+  // ------------------------------------------------------------------------------------------------
+
+  app.get("/", async () => {
+    const transaction = await knexDb("transaction").select();
+
+    return { transaction };
+  });
+
+  app.get("/:id", async (request) => {
+    /* 
+    Para relembrarmos, aqui estou fazendo o seguinte:
+    - Tipando nosso ID com ZOD
+    - Parseando (transformando em um objeto estruturado) nossos parametros, exepecifico o ID
+    - Fazendo a consulta no banco onde tenha apenas esse ID (o .first garante que ele seja o único e primeiro) 
+    */
+    const getTransactionParamsSchema = z.object({
+      id: string().uuid(),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(request.params);
+
+    const transaction = await knexDb("transaction").where("id", id).first();
+
+    return {
+      transaction,
+    };
+  });
+
+  app.get("/summary", async () => {
+    /* 
+    Aqui apenas criamos a rota que retornará uma soma de todas as transações,
+    mas ago interessante são as definições de .first que ao Knex entender que o 
+    retorno será um só ele remove a obrigatoriedade de ser um array, e também
+    definimos um nome especifico a coluna
+     */
+    const summary = await knexDb("transaction")
+      .sum("amount", { as: "amount" })
+      .first();
+
+    return {
+      summary,
+    };
+  });
+
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
